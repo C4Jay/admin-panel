4d703622cac02169f5571ed3548545151c176d29
C4Jay <choolakejay@gmail.com>
2019-11-20 09:45:01 +0530

    2-of-em

--- src/components/HomeChrt.vue
+++ src/components/HomeChrt.vue
@@ -1,9 +1,26 @@
 <template>
+<v-container>
+    <v-flex xs-12>
+        <v-row>
+    <v-card elevation="19" height="209" width="408" color="purple">
     <pure-vue-chart
   :points="[3,5,2,5,4]"
   :width="400"
   :height="200"
 />
+<v-card-text>number of profiles</v-card-text>
+    </v-card>
+    <v-card elevation="19" height="209" width="408" color="purple">
+    <pure-vue-chart
+  :points="[3,5,2,5,4]"
+  :width="400"
+  :height="200"
+/>
+<v-card-text>number of profiles</v-card-text>
+    </v-card>
+        </v-row>
+    </v-flex>
+</v-container>
 </template>
 
 <script>
