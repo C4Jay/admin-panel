bdc9e7e2ba5056aaa04085789cdc6eb271111a3c
C4Jay <choolakejay@gmail.com>
2019-11-20 09:38:31 +0530

    working-chrt

---
+++ src/components/HomeChrt.vue
@@ -0,0 +1,19 @@
+<template>
+    <pure-vue-chart
+  :points="[3,5,2,5,4]"
+  :width="400"
+  :height="200"
+/>
+</template>
+
+<script>
+import PureVueChart from 'pure-vue-chart';
+
+export default {
+    name: 'HomeChrt',
+
+components: {
+    PureVueChart,
+},
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
+import PureVueChart from 'pure-vue-chart';
 
 Vue.config.productionTip = false
 
+Vue.use(PureVueChart);
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
